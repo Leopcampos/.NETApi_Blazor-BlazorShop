1dfda3f1fa17fc7e8acd2b69928537a7eea7c896
Leonardo Pereira Campos <leonardocampos@fiotec.fiocruz.br>
2023-10-04 16:28:11 -0300

    Final da Aula 2, criando a Interface de produto e seus métodos, feito mapeamento das entidades para o Dto e criando alguns métodos no ProdutosController.

--- BlazorShop/BlazorShop.Api/Program.cs
+++ BlazorShop/BlazorShop.Api/Program.cs
@@ -1,4 +1,5 @@
 using BlazorShop.Api.Context;
+using BlazorShop.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- BlazorShop/BlazorShop.Api/Repositories/IProdutoRepository.cs
+++ BlazorShop/BlazorShop.Api/Repositories/IProdutoRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace BlazorShop.Api.Repositories
+﻿using BlazorShop.Api.Entities;
+
+namespace BlazorShop.Api.Repositories
 {
     public interface IProdutoRepository
     {
+        Task<IEnumerable<Produto>> GetItens();
+        Task<Produto> GetItem(int id);
+        Task<IEnumerable<Produto>> GetItensPorCategoria(int id);
     }
 }
