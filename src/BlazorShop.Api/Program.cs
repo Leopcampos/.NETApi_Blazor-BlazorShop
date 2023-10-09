8ae2f078b850dcc603baf5d1801afc4eaddbc547
Leo Campos <leopcampos85@gmail.com>
2023-10-09 20:42:34 -0300

    Finalizando a parte de incluir produtos no carrinho e salvando na tabela CarrinhoItem do banco de dados.

--- src/BlazorShop.Api/Program.cs
+++ src/BlazorShop.Api/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<ICarrinhoCompraRepository, CarrinhoCompraRepository>();
 
 builder.Services.AddCors();
 
